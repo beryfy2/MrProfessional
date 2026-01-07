8abbe8a17cae88436bb412ac31a99866a877be90
Off-Aditya <official.aditya.singh.343@gmail.com>
2026-01-07 17:48:28 +0530

    video

--- frontend/src/pages/HeroPage.jsx
+++ frontend/src/pages/HeroPage.jsx
@@ -27,7 +27,7 @@ export default function HeroPage() {
             <div className="fixed right-6 bottom-6 z-999 flex flex-col gap-4">
                 <a
                     href="tel:88009 32090"
-                    className="h-14 w-14 rounded-full bg-[var(--color-brand)] flex items-center justify-center text-white shadow-lg hover:scale-110 transition"
+                    className="h-14 w-14 rounded-full bg-(--color-brand) flex items-center justify-center text-white shadow-lg hover:scale-110 transition"
                 >
                     <FontAwesomeIcon icon={faPhone} />
                 </a>
