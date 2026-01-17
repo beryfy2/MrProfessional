ff9d80eb7b672cc857ed35146e0d5a064c3a93df
shailjashukla <shubhishukla016@gmail.com>
2026-01-18 00:51:07 +0530

    bkl

--- frontend/src/pages/AchievementPage.jsx
+++ frontend/src/pages/AchievementPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import NavBar from "../components/NavBar";
+import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../style/achievements.css";
 
